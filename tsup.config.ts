7817d5eff9adf733e27e01dbed29facc5f78833a
Henrique <henrique@sidekick-coder.com>
2025-06-14 14:17:52 -0300

    fix: build files structure

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,13 +1,45 @@
 import { defineConfig } from 'tsup'
-import path from 'path'
+import { dirname, relative, resolve } from 'path'
+import fg from 'fast-glob'
+import fs from 'fs'
 
 export default defineConfig({
+    tsconfig: resolve(__dirname, 'tsconfig.json'),
     entry: ['src/**/**/*.ts', '!src/**/**/*.spec.ts'],
-    format: 'esm',
-    tsconfig: path.resolve(__dirname, 'tsconfig.json'),
-    splitting: true,
-    clean: true,
-    treeshake: true,
     external: ['fast-glob', 'yaml', 'chalk'],
+    format: 'esm',
     platform: 'node',
+    splitting: false,
+    clean: true,
+    treeshake: false,
+    sourcemap: false,
+    minify: false,
+    skipNodeModulesBundle: true,
+    bundle: false,
+    async onSuccess() {
+        // fix alias
+        const files = await fg('dist/**/*.js', { onlyFiles: true })
+        const alias: Record<string, string> = {}
+
+        for (const folder of fs.readdirSync(resolve('dist'))) {
+            alias[`@/${folder}`] = resolve('dist', folder)
+        }
+
+        console.log(alias)
+
+        for (const file of files.reverse()) {
+            const folder = dirname(file)
+            let content = await fs.promises.readFile(file, 'utf-8')
+
+            for (const [key, value] of Object.entries(alias)) {
+                let importPath = relative(folder, value).replace(/\\/g, '/')
+
+                importPath = './' + importPath
+
+                content = content.replace(new RegExp(key, 'g'), importPath)
+            }
+
+            await fs.promises.writeFile(file, content)
+        }
+    },
 })
