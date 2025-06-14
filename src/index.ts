dc4e82322dfb5ce2740c134a28786ea394d01548
Henrique <henrique@sidekick-coder.com>
2025-06-14 10:53:24 -0300

    feat: load lab from config & pwd

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,89 @@
 import { createCommander, createCommanderHelp } from '@/core/commander/index.js'
+import fs from 'fs'
 import files from './modules/files/index.js'
 import sources from './modules/sources/index.js'
 import items from './modules/items/index.js'
+import config from './modules/config/index.js'
+import ConfigRepository from './modules/config/ConfigRepository.js'
+import { tryCatch } from './utils/tryCatch.js'
+import { resolve } from 'path'
+import { pathToFileURL } from 'url'
 
-export const commander = createCommander({
-    name: 'lab',
-    bin: 'lab',
-    defaultCommand: 'help',
-})
+function importModule(filename: string) {
+    const url = pathToFileURL(filename)
 
-commander.add(createCommanderHelp(commander))
-
-const labs = [files, sources, items]
-
-for (const lab of labs) {
-    lab.setup({ commander })
+    return import(url.href)
 }
 
-// add modules
-//import './modules/files/index.js'
-//import './modules/items/index.js'
-//import './modules/sources/index.js'
+async function main() {
+    const commander = createCommander({
+        name: 'lab',
+        bin: 'lab',
+        defaultCommand: 'help',
+    })
 
-commander.handle(process.argv.slice(2))
+    commander.add(createCommanderHelp(commander))
+
+    const labs = [config]
+
+    // config
+    const configRepository = new ConfigRepository()
+
+    const configs = await configRepository.list()
+    const configLabs = configs.map((config) => config.labs || []).flat()
+
+    const namedLabs = {
+        files,
+        sources,
+        items,
+    }
+
+    for (const lab of configLabs) {
+        if (namedLabs[lab]) {
+            labs.push(namedLabs[lab])
+            continue
+        }
+
+        const exists = fs.existsSync(lab)
+
+        if (!exists) {
+            console.warn(`Lab module "${lab}" does not exist. Skipping...`)
+            continue
+        }
+
+        const [error, labModule] = await tryCatch(() => import(lab))
+
+        if (error) {
+            console.error(`Failed to load lab module "${lab}":`, error)
+            continue
+        }
+
+        labs.push(labModule.default || labModule)
+    }
+
+    // pwd lab
+    if (fs.existsSync(resolve(process.cwd(), 'lab.mjs'))) {
+        const [error, pwdLab] = await tryCatch(() =>
+            importModule(resolve(process.cwd(), 'lab.mjs'))
+        )
+
+        if (error) {
+            console.error('Failed to load local lab module:', error)
+        }
+
+        if (!error && pwdLab.default) {
+            labs.push(pwdLab.default)
+        }
+    }
+
+    for (const lab of labs) {
+        lab.setup({ commander })
+    }
+
+    commander.handle(process.argv.slice(2))
+}
+
+main().catch((error) => {
+    console.error('Error:', error)
+    process.exit(1)
+})
